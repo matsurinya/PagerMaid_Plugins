30061665cc825cf5e7c4a64dbe6411efbefc05c4
lowking <lowking@sina.cn>
2021-10-30 23:02:05 +0800

    feat(func): forward.py support png file

--- keyword_func/forward.py
+++ keyword_func/forward.py
@@ -49,6 +49,9 @@ async def main(context, sender_ids, forward_target):
                 await sleep(5)
                 await smsg.delete()
                 return ""
+            isShowDetail = "sdtl" in context.text
+            if isShowDetail:
+                await context.client.send_message(context.chat_id, f"以下是目标消息：{reply}")
             sourceCmd = context.text[1:].strip()
             isSourceCmdNotEmpty = False
             if sourceCmd != "":
@@ -61,6 +64,7 @@ async def main(context, sender_ids, forward_target):
                 isNeedDeal = True
                 resultMsg = make_reply_msg(context, target.text).strip()
                 if target.media is not None and not isinstance(target.media, MessageMediaWebPage):
+                    mediaType = target.media.document.mime_type.split('/')
                     if isinstance(target.media, MessageMediaPhoto):
                         await context.edit(f"{sourceCmd}识别到是图片，正在处理。。。")
                         # 图片类型
@@ -70,7 +74,7 @@ async def main(context, sender_ids, forward_target):
                             photo.name = f"../forward-photo.png"
                             if exists(photo.name):
                                 photo.name = f"../suffix-{photo.name}"
-                            await context.edit(f"{sourceCmd}文件下载中。。。")
+                            await context.edit(f"{sourceCmd}图片下载中。。。")
                             await bot.download_media(target.photo, photo)
                             with open(photo.name, "wb") as f:
                                 f.write(photo.getvalue())
@@ -80,11 +84,36 @@ async def main(context, sender_ids, forward_target):
                             remove(photo.name)
                         else:
                             isNeedDeal = False
-                    elif "image" in target.media.document.mime_type.split('/'):
+                    elif "png" in mediaType:
+                        # 图片文件
+                        await context.edit(f"{sourceCmd}识别到图片文件，正在处理。。。")
+                        isNeedDeal = True
+                        if resultMsg != "" and isSourceCmdNotEmpty:
+                            file = BytesIO()
+                            # 遍历获取文件名
+                            for attr in target.media.document.attributes:
+                                try:
+                                    file.name = f"../{attr.file_name}"
+                                    break
+                                except:
+                                    pass
+                            if exists(file.name):
+                                file.name = f"../suffix-{file.name}"
+                            await context.edit(f"{sourceCmd}图片文件下载中。。。")
+                            await bot.download_file(target.media.document, file)
+                            with open(file.name, "wb") as f:
+                                f.write(file.getvalue())
+                            await context.edit(f"{sourceCmd}下载图片文件完成")
+                            await bot.send_file(forward_target, file.name, caption=resultMsg, force_document=True)
+                            await context.edit(f"转发成功")
+                            remove(file.name)
+                        else:
+                            isNeedDeal = False
+                    elif "webp" in mediaType or "x-tgsticker":
                         # 贴纸
                         await context.edit(f"{sourceCmd}识别到贴纸，准备转发。。。")
                         isNeedDeal = False
-                    elif target.media.document.mime_type.split('/') != "":
+                    elif mediaType != "":
                         # 脚本文件
                         await context.edit(f"{sourceCmd}识别到文件，正在处理。。。")
                         isNeedDeal = True
